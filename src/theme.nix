97d7bf2b1aa36ca4b4c1e118733101e17069b676
likivik <likivik@gmail.com>
2024-07-31 09:05:15 +0300

    - big chunk of fixes for the dark variant - reformat color definition to align with documentation

---
+++ src/theme.nix
@@ -0,0 +1,39 @@
+
+
+general = {
+    foreground = "#F0F0F0";
+    foreground-secondary = "#fdfdfd";
+    highlight-transparent = "#f0f0f009";
+    background = "#1C1C1C";
+    background-secondary =  "#2c2c2c";
+    border = "#3c3c3c";
+    border-secondary = "#868686";
+    shadow = "#151515";
+};
+
+special = {
+    purple-main = "#7347d1";
+    purple-darker = "#7347d1cf";
+    purple-pale = "#b299ec";
+};
+
+
+purple = {
+    border = "#b299ec";
+    background = "#b299ec5a";
+};
+
+errors = {
+    border = "#f01313";
+    background = "#692121";
+};
+
+warning = {
+    border = "#e9a700";
+    background = "#715204";
+};
+
+info = {
+    border = "#75beff";
+    background = "#355978";
+};
